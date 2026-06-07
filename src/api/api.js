e8d04b125a246d425021f1e9780f7119a75572cf
Rahul Takale <rahultakale698@gmailcom>
2026-06-07 20:19:41 +0530

    Connect frontend to live backend

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://campuscare-backend-rt14.onrender.com/api",
 });
 
 export default api;
